2d6fa9858a1d3ff1d52a1b584ef84e84cf2602f4
FcoJavierJim <fcojavierjim01@gmail.com>
2026-08-20 16:48:56 +0200

    menú de navegación traducido con botones ingles y español

--- js/script.js
+++ js/script.js
@@ -15,3 +15,49 @@ enlaces.forEach(enlace => {
         enlacesNavegacion.classList.remove("activo");
     });
 });
+
+
+// =========================
+// IDIOMAS
+// =========================
+
+const traducciones = {
+    es: {
+        "nav.inicio": "Inicio",
+        "nav.sobreMi": "Sobre mí",
+        "nav.tecnologias": "Tecnologías",
+        "nav.proyectos": "Proyectos",
+        "nav.experiencia": "Experiencia",
+        "nav.contacto": "Contacto"
+    },
+
+    en: {
+        "nav.inicio": "Home",
+        "nav.sobreMi": "About me",
+        "nav.tecnologias": "Technologies",
+        "nav.proyectos": "Projects",
+        "nav.experiencia": "Experience",
+        "nav.contacto": "Contact"
+    }
+};
+
+function cambiarIdioma(idioma) {
+
+    const elementos = document.querySelectorAll("[data-i18n]");
+
+    elementos.forEach(elemento => {
+
+        const clave = elemento.getAttribute("data-i18n");
+
+        elemento.textContent = traducciones[idioma][clave];
+
+    });
+}
+
+document.getElementById("btn-es").addEventListener("click", () => {
+    cambiarIdioma("es");
+});
+
+document.getElementById("btn-en").addEventListener("click", () => {
+    cambiarIdioma("en");
+});
